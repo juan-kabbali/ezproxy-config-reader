3abb33c9f6a4f51e70b37f355917091d89a118f0
Juan-Kabbali <juanpablomartinez590@hotmail.com>
2017-12-11 18:32:46 -0500

    conn passed to mysql_error()

--- Assets/SqlManager.php
+++ Assets/SqlManager.php
@@ -33,21 +33,21 @@ function GenerateSQL(array $stanzas_array, $account_value, $mysqluser, $mysqlpas
 
         // INSERT DATABASES
         $sql_adm_basedatos = "INSERT INTO adm_basedatos ( id, titulo, url) VALUES (". $stanza->db_var.",".$stanza->title.",".$stanza->url.")";
-        mysqli_query($conn, $sql_adm_basedatos) or die(mysqli_error());
+        mysqli_query($conn, $sql_adm_basedatos) or die(mysqli_error($conn));
         echo $stanza->title." created successfully \n";
         //echo 'INSERT INTO adm_basedatos ( id, titulo, url) VALUES (' . $stanza->db_var . ', ' . $stanza->title . ', ' . $stanza->url . '); <br>';
 
 
         // ADD IT TO ID ACCOUNT
         $sql_cuentas_x_basedatos = "INSERT INTO cuentas_x_basedatos (cuenta_id, basedatos_id) VALUES (".$account_value.",".$stanza->db_var.")";
-        mysqli_query($sql_cuentas_x_basedatos, $conn) or die(mysqli_error());
+        mysqli_query($sql_cuentas_x_basedatos, $conn) or die(mysqli_error($conn));
         echo $stanza->title." added to account ".$account_value."\n";
         //echo 'INSERT INTO cuentas_x_basedatos (cuenta_id, basedatos_id) VALUES (' .$account_value . ',' . $stanza->db_var . '); <br>';
 
         foreach ($stanza->patterns as $pattern) {
             // ADD PATTERNS TO DATABASE
             $sql_adm_basedatos_patrones = "INSERT INTO adm_basedatos_patrones ( basedatos_id, patron) VALUES (".$stanza->db_var.",".$pattern.")";
-            mysqli_query($sql_adm_basedatos_patrones, $conn) or die(mysqli_error());
+            mysqli_query($sql_adm_basedatos_patrones, $conn) or die(mysqli_error($conn));
             echo $pattern." added to database ".$stanza->title."\n";
             //echo 'INSERT INTO adm_basedatos_patrones ( basedatos_id, patron) VALUES (' . $stanza->db_var . ', ' . $pattern . '); <br>';
         }
